fb1c84871bacdf597490ca98550604e9f501f9f4
Neeraj Nerkar <79716367+nickname8888@users.noreply.github.com>
2022-10-11 22:26:01 +0530

    added emotion recognition

--- main.py
+++ main.py
@@ -6,11 +6,62 @@ import numpy as np
 import time
 import tempfile
 from PIL import Image
+from tensorflow import keras
+from keras.models import model_from_json
+from keras.preprocessing.image import img_to_array
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 DEMO_IMAGE = 'demo.jpeg'
 DEMO_VIDEO = 'demo.avi'
+
+# load model
+emotion_dict = {0:'angry', 1 :'happy', 2: 'neutral', 3:'sad', 4: 'surprise'}
+# load json and create model
+json_file = open('emotion_model1.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+classifier = model_from_json(loaded_model_json)
+
+# load weights into new model
+classifier.load_weights("emotion_model1.h5")
+
+#load face
+try:
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+except Exception:
+    st.write("Error loading cascade classifiers")
+
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+
+class Faceemotion(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        #image gray
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            image=img_gray, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(255, 0, 0), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype('float') / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
+                prediction = classifier.predict(roi)[0]
+                maxindex = int(np.argmax(prediction))
+                finalout = emotion_dict[maxindex]
+                output = str(finalout)
+            label_position = (x, y)
+            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        return img
+
 
 st.title('Face Mesh Application')
 
@@ -53,7 +104,7 @@ def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     return resized
 
 app_mode = st.sidebar.selectbox('Choose the App Mode', 
-                                   ['About', 'Run on Image', 'Run on Video']
+                                   ['About', 'Run on Image', 'Run on Video', 'Webcam Face Detection']
                                    )
 
 if app_mode == 'About': 
@@ -77,7 +128,7 @@ if app_mode == 'About':
     )
 
 elif app_mode == 'Run on Image': 
-    drawing_spec = mp_drawing.DrawingSpec(thickness=2, circle_radius=1)
+    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
     
     st.sidebar.markdown('---')
     
@@ -269,4 +320,12 @@ elif app_mode == 'Run on Video':
                 frame = cv2.resize(frame, (0,0), fx=0.8, fy=0.8)
                 frame = image_resize(image=frame, width=640)
                 stframe.image(frame, channels='BGR', use_column_width=True)
-        
+
+elif app_mode == 'Webcam Face Detection': 
+    st.header("Webcam Live Feed")
+    st.write("Click on start to use webcam and detect your face emotion")
+    webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION,
+                    video_processor_factory=Faceemotion)
+
+    
+    
